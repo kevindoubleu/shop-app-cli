ce823cd365965844500773015c88756b771659fc
Kevin Wibamanto <kevin.wibamanto@gmail.com>
2024-12-24 16:22:55 +0700

    user input string

--- shop.cpp
+++ shop.cpp
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-int main() {
-    printf("hi");
+void main() {
+    printf("#############################\n");
+    printf("#  Welcome to kevin's shop  #\n");
+    printf("#############################\n");
+    printf("mau pesen apa bro\n");
+    printf("1. nasi\n");
+    printf("2. bakmi\n");
+
+    char user_input[1];
+    printf("hmm gw mau ");
+    scanf("%s", &user_input);
+    printf("oh lu mau pesen %s oke\n", user_input);
 }
