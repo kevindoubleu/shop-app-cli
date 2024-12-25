52d96916b7dd6b3d1feb79765479cc7a6d2eb903
Kevin Wibamanto <kevin.wibamanto@gopay.co.id>
2024-12-26 00:38:22 +0700

    if else statements

--- shop.cpp
+++ shop.cpp
@@ -11,5 +11,17 @@ int main() {
     int user_input = 0;
     printf("hmm gw mau ");
     scanf("%d", &user_input);
-    printf("oh lu mau pesen nomor %d oke\n", user_input);
+
+    if (user_input == 1)
+    {
+        printf("oh lu mau pesen nasi oke\n");
+    }
+    else if (user_input == 2)
+    {
+        printf("oh lu mau pesen bakmi oke\n");
+    }
+    else
+    {
+        printf("lu mau pesen apa sih, nomor 1 ato 2 pilih\n");
+    }
 }
