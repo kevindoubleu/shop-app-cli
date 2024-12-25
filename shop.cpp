fdebf784ecbf11f08b80f70b20d2412f3107e63f
Kevin Wibamanto <kevin.wibamanto@gopay.co.id>
2024-12-26 00:41:13 +0700

    switch case

--- shop.cpp
+++ shop.cpp
@@ -9,19 +9,19 @@ int main() {
     printf("2. bakmi\n");
 
     int user_input = 0;
-    printf("hmm gw mau ");
+    printf("hmm gw mau nomor ");
     scanf("%d", &user_input);
 
-    if (user_input == 1)
+    switch (user_input)
     {
-        printf("oh lu mau pesen nasi oke\n");
-    }
-    else if (user_input == 2)
-    {
-        printf("oh lu mau pesen bakmi oke\n");
-    }
-    else
-    {
-        printf("lu mau pesen apa sih, nomor 1 ato 2 pilih\n");
+        case 1:
+            printf("oh lu mau pesen nasi oke\n");
+            break;
+        case 2:
+            printf("oh lu mau pesen bakmi oke\n");
+            break;
+        default:
+            printf("lu mau pesen apa sih, nomor 1 ato 2 pilih\n");
+            break;
     }
 }
