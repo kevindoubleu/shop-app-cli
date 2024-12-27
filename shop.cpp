5c740b8b263c1841f958932f223c42e07cbc68e7
Kevin Wibamanto <kevin.wibamanto@gopay.co.id>
2024-12-27 10:03:51 +0700

    cancel using switch case

--- shop.cpp
+++ shop.cpp
@@ -7,25 +7,26 @@ int main() {
     printf("mau pesen apa dek\n");
     printf("1. nasi\n");
     printf("2. bakmi\n");
-    printf("2. es krim\n");
+    printf("3. es krim\n");
 
     int user_input = 0;
     printf("hmm gw mau nomor ");
     scanf("%d", &user_input);
-
-    switch (user_input)
+    
+    if (user_input == 1)
     {
-        case 1:
-            printf("oh lu mau pesen nasi oke\n");
-            break;
-        case 2:
-            printf("oh lu mau pesen bakmi oke\n");
-            break;
-        case 3:
-            printf("oh lu mau es krim aja oke\n");
-            break;
-        default:
-            printf("lu mau pesen apa sih, pilih nomor 1-3\n");
-            break;
+        printf("oh lu mau pesen nasi oke\n");
+    }
+    else if (user_input == 2)
+    {
+        printf("oh lu mau pesen bakmi oke\n");
+    }
+    else if (user_input == 3)
+    {
+        printf("oh lu mau es krim aja oke\n");
+    }
+    else
+    {
+        printf("lu mau pesen apa sih, pilih nomor 1-3\n");
     }
 }
