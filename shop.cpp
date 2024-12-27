9c9c42344b78a2e395e5d533302db73ccdde1582
Kevin Wibamanto <kevin.wibamanto@gopay.co.id>
2024-12-27 10:25:36 +0700

    basic while loop

--- shop.cpp
+++ shop.cpp
@@ -10,23 +10,29 @@ int main() {
     printf("3. es krim\n");
 
     int user_input = 0;
-    printf("hmm gw mau nomor ");
-    scanf("%d", &user_input);
-    
-    if (user_input == 1)
+    // selama user input diluar dari yg kita expect
+    while (user_input < 1 || user_input > 3)
     {
-        printf("oh lu mau pesen nasi oke\n");
-    }
-    else if (user_input == 2)
-    {
-        printf("oh lu mau pesen bakmi oke\n");
-    }
-    else if (user_input == 3)
-    {
-        printf("oh lu mau es krim aja oke\n");
-    }
-    else
-    {
-        printf("lu mau pesen apa sih, pilih nomor 1-3\n");
+        // tanya user mau apa
+        printf("hmm gw mau nomor ");
+        scanf("%d", &user_input);
+
+        // kasih feedback ke user tentang pilihannya
+        if (user_input == 1)
+        {
+            printf("oh lu mau pesen nasi oke\n");
+        }
+        else if (user_input == 2)
+        {
+            printf("oh lu mau pesen bakmi oke\n");
+        }
+        else if (user_input == 3)
+        {
+            printf("oh lu mau es krim aja oke\n");
+        }
+        else
+        {
+            printf("lu mau pesen apa sih, pilih nomor 1-3\n");
+        }
     }
 }
