e9c5031a3c8b864e7ac771bfcc060c2fd2fb49d6
Kevin Wibamanto <kevin.wibamanto@gopay.co.id>
2024-12-27 10:33:27 +0700

    exitting while loop

--- shop.cpp
+++ shop.cpp
@@ -8,6 +8,7 @@ int main() {
     printf("1. nasi\n");
     printf("2. bakmi\n");
     printf("3. es krim\n");
+    printf("4. exit, ga jadi\n");
 
     int user_input = 0;
     // selama user input diluar dari yg kita expect
@@ -30,9 +31,16 @@ int main() {
         {
             printf("oh lu mau es krim aja oke\n");
         }
+        else if (user_input == 4)
+        {
+            printf("oh ga jadi yaudah gpp\n");
+            break;
+        }
         else
         {
             printf("lu mau pesen apa sih, pilih nomor 1-3\n");
         }
     }
+
+    printf("terima kasih udah dateng ke my warteg\n");
 }
